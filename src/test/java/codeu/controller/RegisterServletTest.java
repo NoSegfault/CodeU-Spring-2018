e393eadfb91942ef21f64e3557c9a90d61bc79eb
gracekfan <gfduri@gmail.com>
2018-03-17 10:41:48 -0500

    Fixed Step 4 test for RegisterServletTest.java

--- src/test/java/codeu/controller/RegisterServletTest.java
+++ src/test/java/codeu/controller/RegisterServletTest.java
@@ -2,6 +2,7 @@ package codeu.controller;
 
 import codeu.model.data.User;
 import codeu.model.store.basic.UserStore;
+import javax.servlet.RequestDispatcher;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -11,44 +12,23 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import codeu.model.data.User;
-import codeu.model.store.basic.UserStore;
-import java.io.IOException;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
-
 public class RegisterServletTest {
+    
+    private RegisterServlet registerServlet;
+    private HttpServletRequest mockRequest;
+    private HttpServletResponse mockResponse;
+    private RequestDispatcher mockRequestDispatcher;
+    
+    @Before
+    public void setup() {
+        registerServlet = new RegisterServlet();
+        mockRequest = Mockito.mock(HttpServletRequest.class);
+        mockResponse = Mockito.mock(HttpServletResponse.class);
+        mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
+        Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/register.jsp"))
+        .thenReturn(mockRequestDispatcher);
+    }
 
- private RegisterServlet registerServlet;
- private HttpServletRequest mockRequest;
- private PrintWriter mockPrintWriter;
- private HttpServletResponse mockResponse;
- private RequestDispatcher mockRequestDispatcher;
-
- @Before
- public void setup() throws IOException {
-   registerServlet = new RegisterServlet();
-   mockRequest = Mockito.mock(HttpServletRequest.class);
-   mockPrintWriter = Mockito.mock(PrintWriter.class);
-   mockResponse = Mockito.mock(HttpServletResponse.class);
-   mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
-   Mockito.when(mockResponse.getWriter()).thenReturn(mockPrintWriter);
- }
-
- // @Test
- // public void testDoGet() throws IOException, ServletException {
- //   registerServlet.doGet(mockRequest, mockResponse);
-
- //   Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
- // }
 
  // @Test
  //  public void testDoPost_BadUsername() throws IOException, ServletException {
